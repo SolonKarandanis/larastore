dda3d346ea7292d4c005f0d0eec5481e580e9c86
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-01-12 08:58:45 +0200

    Added Category Model and migration

--- app/Filament/Resources/DepartmentResource.php
+++ app/Filament/Resources/DepartmentResource.php
@@ -66,7 +66,7 @@ class DepartmentResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            RelationManagers\CategoriesRelationManager::class
         ];
     }
 
