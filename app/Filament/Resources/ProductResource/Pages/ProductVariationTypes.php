bd9a46c51b304359890312a89067d84116f171ec
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-01-15 08:05:54 +0200

    Implemented Variation types page

--- app/Filament/Resources/ProductResource/Pages/ProductVariationTypes.php
+++ app/Filament/Resources/ProductResource/Pages/ProductVariationTypes.php
@@ -2,8 +2,13 @@
 
 namespace App\Filament\Resources\ProductResource\Pages;
 
+use App\Enums\ProductVariationTypesEnum;
 use App\Filament\Resources\ProductResource;
 use Filament\Actions;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Pages\EditRecord;
 
@@ -11,13 +16,48 @@ class ProductVariationTypes extends EditRecord
 {
     protected static string $resource = ProductResource::class;
 
-    protected static ?string $navigationIcon = 'heroicon-c-photo';
+    protected static ?string $navigationIcon = 'heroicon-m-numbered-list';
+
+    protected static ?string $title = 'Variation Types';
 
     public function form(Form $form): Form
     {
         return $form
                 ->schema([
-
+                    Repeater::make('variationTypes')
+                        ->label(false)
+                        ->relationship()
+                        ->collapsible()
+                        ->defaultItems(1)
+                        ->addActionLabel('Add Variation Type')
+                        ->columns(2)
+                        ->columnSpan(2)
+                        ->schema([
+                            TextInput::make('name')
+                                ->required(),
+                            Select::make('type')
+                                ->options(ProductVariationTypesEnum::labels())
+                                ->required(),
+                            Repeater::make('options')
+                                ->relationship()
+                                ->collapsible()
+                                ->schema([
+                                    TextInput::make('name')
+                                        ->columnSpan(2)
+                                        ->required(),
+                                    SpatieMediaLibraryFileUpload::make('images')
+                                        ->image()
+                                        ->multiple()
+                                        ->openable()
+                                        ->panelLayout('grid')
+                                        ->collection('images')
+                                        ->reorderable()
+                                        ->appendFiles()
+                                        ->preserveFilenames()
+                                        ->columnSpan(3)
+                                ])
+                                ->columnSpan(2)
+                        ])
                 ]);
     }
 
