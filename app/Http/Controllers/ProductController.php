09aaffe61b6290062100ff4bb2e629b7ca375e78
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-01-16 15:58:43 +0200

    Implementing Product Listing

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ProductListResource;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ProductController extends Controller
 {
-    //
+    public function index(){
+        $products = Product::query()
+            ->published()
+            ->paginate(12);
+        return Inertia::render('Home', [
+            'products' => ProductListResource::collection($products)
+        ]);
+    }
 }
