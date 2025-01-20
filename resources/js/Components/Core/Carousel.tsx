f098a6b1c4666e8db5dfa9f42573c3f8cd1edcf1
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-01-20 14:04:45 +0200

    Implemented Product details

--- resources/js/Components/Core/Carousel.tsx
+++ resources/js/Components/Core/Carousel.tsx
@@ -1,25 +1,31 @@
-import {FC} from 'react'
+import {FC, useEffect, useState} from 'react'
 import {Image} from "@/types";
 
 interface Props{
   images:Image[];
 }
 const Carousel:FC<Props> = ({images}) => {
+  const [selectedImage,setSelectedImage] = useState<Image>(images[0]);
+
+  useEffect(()=>{
+    setSelectedImage(images[0]);
+  },[images])
+
   return (
     <div className="flex items-start gap-8">
       <div className="flex flex-col items-center gap-2 py-2">
         {images.map((image,i)=>(
-          <a key={image.id} href={`#item${i}`} className="border-2 hover:border-blue-500">
+          <button
+            onClick={ev=>setSelectedImage(image)}
+             key={image.id} className="border-2 hover:border-blue-500">
             <img src={image.thumb} alt="" className="w-[50px]"/>
-          </a>
+          </button>
         ))}
       </div>
       <div className="carousel w-full">
-        {images.map((image,i)=>(
-          <div id={`item${i}`} key={image.id} className="carousel-item w-full">
-            <img src={image.large} alt=""  className="w-full"/>
-          </div>
-        ))}
+        <div className="carousel-item w-full">
+          <img src={selectedImage.large} alt="" className="w-full"/>
+        </div>
       </div>
     </div>
   )
