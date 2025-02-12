fbf698d87b71707e427b3fdb469630f6e56e6663
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-02-12 14:28:46 +0200

    Implementing Payout to vendors

--- app/Console/Commands/PayoutVendors.php
+++ app/Console/Commands/PayoutVendors.php
@@ -2,9 +2,12 @@
 
 namespace App\Console\Commands;
 
+use App\Enums\OrderStatusEnum;
+use App\Models\Order;
 use App\Models\Payout;
 use App\Models\Vendor;
 use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class PayoutVendors extends Command
@@ -47,9 +50,33 @@ class PayoutVendors extends Command
             $startingFrom = Payout::where('vendor_id',$vendor->user_id)
                 ->orderBy('until','desc')
                 ->value('until');
+            $startingFrom = $startingFrom ?: Carbon::make('1970-01-01');
+
+            $until = Carbon::now()->subMonthNoOverflow()->startOfMonth();
+
+            $vendorSubtotal = Order::query()
+                ->where('vendor_user_id', $vendor->user_id)
+                ->where('status', OrderStatusEnum::Paid->value)
+                ->whereBetween('created_at', [$startingFrom, $until])
+                ->sum('vendor_subtotal');
+            if($vendorSubtotal){
+                $this->info('Payout made with amount: '. $vendorSubtotal);
+                Payout::create([
+                    'vendor_id'=>$vendor->user_id,
+                    'amount'=>$vendorSubtotal,
+                    'starting_from'=>$startingFrom,
+                    'until'=>$until,
+                ]);
+                $vendor->user->transfer((int)($vendorSubtotal * 100),config('app.currency'));
+            }
+            else{
+                $this->info('Nothing to process');
+            }
+            DB::commit();
         }
         catch (\Exception $exception){
-
+            DB::rollBack();
+            $this->error($exception->getMessage());
         }
     }
 }
